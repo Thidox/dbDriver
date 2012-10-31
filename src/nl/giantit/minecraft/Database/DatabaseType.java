849456bebd1db2d453432bac1b68a653a63a3dd8
Giant <giant@giantit.nl>
2012-10-31 04:58:51 +0100

    Give database types a name.

--- src/nl/giantit/minecraft/Database/DatabaseType.java
+++ src/nl/giantit/minecraft/Database/DatabaseType.java
@@ -6,8 +6,18 @@ package nl.giantit.minecraft.Database;
  */
 public enum DatabaseType {
 	
-	SQLite(),
-	MySQL(),
-	hTwo()
+	SQLite("SQLite"),
+	MySQL("MySQL"),
+	hTwo("h2");
+	
+	private String name;
+	
+	private DatabaseType(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 	
 }
